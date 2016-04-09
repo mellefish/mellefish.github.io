a9abdadd60c9d68aabf71f4e218b9c2681cf2230
mellefish <michellefong93@gmail.com>
2016-04-08 23:55:55 -0700

    Added files via upload

---
+++ lcc/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('e7yBQuX8qzqBb256UVlYys7Gm','e7yBQuX8qzqBb256UVlYys7Gm');
+    define('lW7fR4tGaIKue39vxfANPUs3UMub1PzTMFRHKXbPxVTh9z9Fi2','lW7fR4tGaIKue39vxfANPUs3UMub1PzTMFRHKXbPxVTh9z9Fi2');
+
+    // User Access Token
+    define('615437092-EQq0AeAtTDIGbIHNNBmcu42bZ24MZniiYP1xVDUM','615437092-EQq0AeAtTDIGbIHNNBmcu42bZ24MZniiYP1xVDUM');
+    define('L5CQ9wdE1U4MvabELu9p7fHrmnfYFno4sdx9xPf6NORhb','L5CQ9wdE1U4MvabELu9p7fHrmnfYFno4sdx9xPf6NORhb');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
